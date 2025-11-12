f3b590c2b7935cb75f34b3cc5caefca3d1ef4871
Szymon <szymko12@interia.pl>
2025-11-12 14:10:34 +0100

    Dodano przycisk

--- app/src/main/java/com/example/sprawdzian01poprawa/MainActivity.java
+++ app/src/main/java/com/example/sprawdzian01poprawa/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sprawdzian01poprawa;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,13 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btn = new Button(this);
+        btn.setText("Kliknij Mnie!!!");
+        btn.setAllCaps(false);
+
+        LinearLayout mainLayout =findViewById(R.id.main);
+        mainLayout.addView(btn);
+
     }
 }
